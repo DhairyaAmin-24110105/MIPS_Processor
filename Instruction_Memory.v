fbd34852cd34394fb1dc39399a751b0809acbc69
DhairyaAmin-24110105 <24110105@iitgn.ac.in>
2026-06-30 21:34:21 +0530

    Create Instruction_Memory.v
    Implement instruction memory module that reads from imem.hex

---
+++ Instruction_Memory.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module Instruction_Memory (
+    input wire [15:0] pc, output wire [15:0] instruction
+);
+    reg [15:0] memory [0:255]; 
+    initial begin
+        $readmemh("imem.hex", memory); //Imem.hex will go here as set of instructions stored in instruction memory
+    end
+    assign instruction = memory[pc[7:0]];
+endmodule
