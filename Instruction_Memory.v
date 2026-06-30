0a05cc27f4938002ded7e4afc6783e00993becf4
DhairyaAmin-24110105 <24110105@iitgn.ac.in>
2026-06-30 21:40:39 +0530

    Update Instruction_Memory.v

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -4,7 +4,8 @@ module Instruction_Memory (
     input wire [15:0] pc, output wire [15:0] instruction
 );
     reg [15:0] memory [0:255]; 
-    initial begin
+    initial 
+    begin
         $readmemh("imem.hex", memory); //Imem.hex will go here as set of instructions stored in instruction memory
     end
     assign instruction = memory[pc[7:0]];
